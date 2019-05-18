ea4d2e62902fda8b8b1fc0852a7d21ef85bce9b4
Ankit Kumar <nktkumar82@gmail.com>
2019-05-18 23:22:13 +0530

    Recipe Api created Category data class needs to be created

---
+++ app/src/main/java/com/captain/ak/zomato/requests/RecipeApi.kt
@@ -0,0 +1,19 @@
+package com.captain.ak.zomato.requests
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+public interface RecipeApi {
+
+
+    //categories
+    @GET("api/v2.1/categories")
+    fun searchCategories(
+        @Header("user-key")key:String):Call<CategoryResponse>
+
+
+
+
+}
---
+++ app/src/main/java/com/captain/ak/zomato/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.captain.ak.zomato.utils
+
+class Constants {
+
+    companion object {
+
+        val BASE_URL:String = "https://developers.zomato.com"
+
+        val NETWORK_TIMEOUT:Int =3000
+
+        val API_KEY:String ="327e75c31ca03dbb55cbabe4257acfa9"
+
+    }
+}
